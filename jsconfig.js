c74734a87c6101f2ac2ec05ff3bd443391a05562
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-10-29 13:29:37 +0100

    make sure only one password error displayed

--- jsconfig.js
+++ jsconfig.js
@@ -3,13 +3,17 @@ const passwordTwo = document.querySelector("#password_2");
 const submitButton = document.querySelector("#mainBtn");
 const errorPwd = document.querySelectorAll(".error");
 
-
-
 submitButton.addEventListener('click', (event) => {
     if (passwordOne.value === passwordTwo.value) {
         return;
     }
     else {
+        errorPwd.forEach(element => {
+            if (element.querySelector('p')) {
+                element.removeChild(element.querySelector('p'));
+            };
+        });
+        
         passwordTwo.value = null;
         passwordOne.style.cssText = "border-color: red"
         passwordTwo.style.cssText = "border-color: red"
